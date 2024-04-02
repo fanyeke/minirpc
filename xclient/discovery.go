c5298ed31fd8770d4e44ffb553bf4b2d2669d618
course <1500982668@qq.com>
2024-04-02 15:41:00 +0800

    负载均衡

--- xclient/discovery.go
+++ xclient/discovery.go
@@ -1,1 +1,86 @@
 package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect SelectMode = iota
+	RoundRobinSelect
+)
+
+type Discovery interface {
+	Refresh() error
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+type MultiServerDiscovery struct {
+	r       *rand.Rand
+	mu      sync.RWMutex
+	servers []string
+	index   int
+}
+
+func NewMultiServerDiscovery(servers []string) *MultiServerDiscovery {
+	d := &MultiServerDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServerDiscovery)(nil)
+
+func (d *MultiServerDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+func (d *MultiServerDiscovery) Refresh() error {
+	return nil
+}
+
+// Get 获取一个服务
+func (d *MultiServerDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	// 没有服务直接报错
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	// 选择模式
+	switch mode {
+	// 随机选择
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	// 轮转法
+	case RoundRobinSelect:
+		// 利用 `index` 字段
+		s := d.servers[d.index%n]
+		// 令 `index` 字段 +1
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// GetAll 返回 `MultiServerDiscovery` 的 `servers` string切片
+func (d *MultiServerDiscovery) GetAll() ([]string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
--- xclient/xclient.go
+++ xclient/xclient.go
@@ -1,1 +1,126 @@
 package xclient
+
+import (
+	"context"
+	"io"
+	"reflect"
+	"sync"
+
+	. "github.com/fanyeke/minirpc"
+)
+
+type XClient struct {
+	d       Discovery
+	mode    SelectMode
+	opt     *Option
+	mu      sync.Mutex
+	clients map[string]*Client
+}
+
+var _ io.Closer = (*Client)(nil)
+
+func NewXClient(d Discovery, mode SelectMode, opt *Option) *XClient {
+	return &XClient{
+		d:       d,
+		mode:    mode,
+		opt:     opt,
+		clients: make(map[string]*Client),
+	}
+}
+
+// Close 关闭当前客户端
+func (xc *XClient) Close() error {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+
+	for key, client := range xc.clients {
+		_ = client.Close()
+		// 记得删除客户端的注册
+		delete(xc.clients, key)
+	}
+	return nil
+}
+func (xc *XClient) dial(rpcAddr string) (*Client, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	// 通过 `rpcAddr` 获取客户端 `client`
+	client, ok := xc.clients[rpcAddr]
+	// 客户端存在但是不可用
+	if ok && !client.IsAvailable() {
+		_ = client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	// 客户端不存在
+	if client == nil {
+		var err error
+		client, err = XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = client
+	}
+	// 返回获取到的客户端
+	return client, nil
+}
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	// 进行连接
+	client, err := xc.dial(rpcAddr)
+	if err != nil {
+		return err
+	}
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	// 通过负载均衡选择一个去处, 获得他的 `rpcAddr`
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	// 调用 call
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+// Broadcast 将请求广播到所有服务实例
+// 1. 请求是并发的
+// 2. 需要使用互斥锁保证 `error` 和 `reply` 被正确赋值
+// 3. 借助 `context.WithCancel` 确保发生错误时快速失败
+func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	servers, err := xc.d.GetAll()
+	if err != nil {
+		return err
+	}
+	// 任务编排
+	var wg sync.WaitGroup
+	// 互斥锁, 实现正确赋值
+	var mu sync.Mutex
+	// 拿到子协程中的错误
+	var e error
+	replyDone := reply == nil
+	// 掌控子协程生命周期
+	ctx, cancel := context.WithCancel(ctx)
+	for _, rpcAddr := range servers {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+			var clonedReply interface{}
+			// 如果 `reply` 不为nil, 则创建一个新的 `reply` 类型的实例赋值给 `clonedReply`
+			if reply != nil {
+				clonedReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := xc.call(rpcAddr, ctx, serviceMethod, args, clonedReply)
+			mu.Lock()
+			if err != nil && e == nil {
+				e = err
+				cancel()
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(clonedReply).Elem())
+				replyDone = true
+			}
+			mu.Unlock()
+		}(rpcAddr)
+	}
+	wg.Wait()
+	return e
+}
